8eeb2359baa073b7f80bc85856a3daa7efd89a1e
dign1120 <dign1120@naver.com>
2025-09-25 00:41:11 +0900

    update : 돌발상황 서비스 내 전체 조회 기능 추가

--- naviSafe-backend/src/main/java/com/naviSafe/naviSafe/domain/outbreak/service/OutbreakService.java
+++ naviSafe-backend/src/main/java/com/naviSafe/naviSafe/domain/outbreak/service/OutbreakService.java
@@ -1,10 +1,22 @@
 package com.naviSafe.naviSafe.domain.outbreak.service;
 
+import com.naviSafe.naviSafe.domain.outbreak.entitiy.Outbreak;
+import com.naviSafe.naviSafe.domain.outbreak.repository.OutbreakRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OutbreakService {
+    private final OutbreakRepository outbreakRepository;
 
+    @Autowired
+    public OutbreakService(OutbreakRepository outbreakRepository) {
+        this.outbreakRepository = outbreakRepository;
+    }
 
-
+    public List<Outbreak> findAll() {
+        return outbreakRepository.findAll();
+    }
 }
