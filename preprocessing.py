f00f4ae796cc513db3d960a7e6c6d9ad9eee0930
dndjeh <dndjeh12@naver.com>
2025-09-25 21:47:58 +0900

    outbreak consumer 전처리

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,6 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, from_unixtime, to_date, concat_ws, to_timestamp
-from pyspark.sql.types import StructType, StructField, StringType, FloatType, IntegerType, DateType
+from pyspark.sql.functions import from_json, col, explode, to_timestamp, when, concat_ws, length
+from pyspark.sql.types import StructType, StructField, StringType, ArrayType
 
 # SparkSession 생성 (Kafka 패키지 포함)
 spark = SparkSession.builder \
@@ -14,7 +14,24 @@ spark = SparkSession.builder \
 
 
 # Kafka에서 읽어올 데이터의 스키마 정의
-schema = StructType([
+# schema = StructType([
+#     StructField("acc_id", StringType(), True),
+#     StructField("link_id", StringType(), True),
+#     StructField("occr_date", StringType(), True),
+#     StructField("occr_time", StringType(), True),
+#     StructField("exp_clr_date", StringType(), True),
+#     StructField("exp_clr_time", StringType(), True),
+#     StructField("acc_type", StringType(), True),
+#     StructField("acc_dtype", StringType(), True),
+#     StructField("acc_info", StringType(), True),
+#     StructField("grs80tm_x", StringType(), True),
+#     StructField("grs80tm_y", StringType(), True),
+#     StructField("acc_road_code", StringType(), True),  # 새로 추가
+# ])
+
+    
+# 스키마 (배열 안에 객체)
+inner_schema = StructType([
     StructField("acc_id", StringType(), True),
     StructField("link_id", StringType(), True),
     StructField("occr_date", StringType(), True),
@@ -23,33 +40,52 @@ schema = StructType([
     StructField("exp_clr_time", StringType(), True),
     StructField("acc_type", StringType(), True),
     StructField("acc_dtype", StringType(), True),
-    StructField("grs80tm_x", FloatType(), True),
-    StructField("grs80tm_y", FloatType(), True),
+    StructField("acc_info", StringType(), True),
+    StructField("grs80tm_x", StringType(), True),
+    StructField("grs80tm_y", StringType(), True),
+    StructField("acc_road_code", StringType(), True),
 ])
 
+schema = ArrayType(inner_schema)
 
-# Kafka 스트리밍 데이터 읽기
+
+
 df_stream = spark.readStream.format("kafka") \
     .option("kafka.bootstrap.servers", "kafka:9092") \
     .option("subscribe", "outbreak_topic") \
     .option("startingOffsets", "earliest") \
     .load()
 
-# JSON 파싱 + timestamp 변환
+
+## 현재 들어오는 값들 출력
+# df_stream.selectExpr("CAST(value AS STRING)").writeStream \
+#     .format("console") \
+#     .start()
+
+# JSON 파싱 + explode
 df_json = df_stream.selectExpr("CAST(value AS STRING) as json_str") \
     .select(from_json(col("json_str"), schema).alias("data")) \
+    .select(explode(col("data")).alias("data")) \
     .select("data.*")
 
 
+# 날짜/시간 필드 변환 (자동 생성된 컬럼을 사용)
 df_json = df_json.withColumn(
-    "OCCR_DATE",
+    "occr_date_time",
     to_timestamp(concat_ws(" ", col("occr_date"), col("occr_time")), "yyyyMMdd HHmm")
 ).withColumn(
-    "EXP_CLR_DATE",
-    to_timestamp(concat_ws(" ", col("exp_clr_date"), col("exp_clr_time")), "yyyyMMdd HHmm")
+    "exp_clr_date_time",
+    when(length(col("exp_clr_time")) == 4,
+         to_timestamp(concat_ws(" ", col("exp_clr_date"), col("exp_clr_time")), "yyyyMMdd HHmm"))
+    .otherwise(to_timestamp(concat_ws(" ", col("exp_clr_date"), col("exp_clr_time")), "yyyyMMdd HHmmss"))
 )
 
-#  배치 단위 처리 함수 정의 (MySQL에 저장)
+# 기존 컬럼 삭제
+df_json = df_json.drop("occr_date", "occr_time", "exp_clr_date", "exp_clr_time")
+
+#df_json.select("OCCR_DATE", "EXP_CLR_DATE").show(5, truncate=False)
+
+#  배치 단위 처리 함수 정의 (MySQL에 저장)EXP_CLR_DATE
 def process_batch(batch_df, batch_id):
     print(f"--- 배치 {batch_id} ---", flush=True)
     batch_df.show(5, truncate=False)
