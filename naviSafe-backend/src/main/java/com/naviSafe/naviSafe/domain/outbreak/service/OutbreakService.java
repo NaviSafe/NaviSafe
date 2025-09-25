1775e89914024524a8413f00df37a9b85f62399b
dign1120 <dign1120@naver.com>
2025-09-25 18:04:28 +0900

    feat : redis에서 돌발정보 조회 및 없을 시 DB로 부터 조회 후 Redis에 저장 기능 추가

--- naviSafe-backend/src/main/java/com/naviSafe/naviSafe/domain/outbreak/service/OutbreakService.java
+++ naviSafe-backend/src/main/java/com/naviSafe/naviSafe/domain/outbreak/service/OutbreakService.java
@@ -3,20 +3,37 @@ package com.naviSafe.naviSafe.domain.outbreak.service;
 import com.naviSafe.naviSafe.domain.outbreak.entitiy.Outbreak;
 import com.naviSafe.naviSafe.domain.outbreak.repository.OutbreakRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.security.Key;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class OutbreakService {
     private final OutbreakRepository outbreakRepository;
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    private static final String KEY = "outbreak:all";
 
     @Autowired
-    public OutbreakService(OutbreakRepository outbreakRepository) {
+    public OutbreakService(OutbreakRepository outbreakRepository, RedisTemplate<String, Object> redisTemplate) {
         this.outbreakRepository = outbreakRepository;
+        this.redisTemplate = redisTemplate;
     }
 
     public List<Outbreak> findAll() {
-        return outbreakRepository.findAll();
+
+        List<Outbreak> cached = (List<Outbreak>) redisTemplate.opsForValue().get(KEY);
+        if(cached != null) {
+            return cached;
+        }
+
+        List<Outbreak> outbreaks = outbreakRepository.findAll();
+
+        redisTemplate.opsForValue().set(KEY, outbreaks, 30, TimeUnit.MINUTES);
+
+        return outbreaks;
     }
 }
