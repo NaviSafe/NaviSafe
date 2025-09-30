a467e7ad67921667e700712e7ee402099b9b3104
dndjeh <dndjeh12@naver.com>
2025-09-30 22:13:15 +0900

    test : 한번만 출력하도록 변경

--- dataflow/utils/check_redis.py
+++ dataflow/utils/check_redis.py
@@ -3,7 +3,6 @@ from redis_utils import RedisClient
 
 redis_client = RedisClient(host="redis", port=6379, db=0)
 
-while True:
-    items = redis_client.lrange("db_queue", 0, -1)
-    print(items)
-    time.sleep(5)
+items = redis_client.lrange("db_queue", 0, -1)
+print(items)
+time.sleep(5)
