d6feceef8bda84574b09df56ee61c72848aa4267
dndjeh <dndjeh12@naver.com>
2025-10-16 21:16:34 +0900

    fix : 테이블명이 틀려서 INSERT가 되지 않는 문제 해결

--- dataflow/airflow/plugins/airflow_utils/outbreak_code_name.py
+++ dataflow/airflow/plugins/airflow_utils/outbreak_code_name.py
@@ -13,47 +13,53 @@ MYSQL_CONFIG = {
 }
 
 # XML 파싱 함수
-def parse_regioninfo(xml_str):
+def parse_outbreak_code_info(xml_str):
     try:
         root = ET.fromstring(xml_str)
         rows = root.findall("row")
-        return [{"acc_type": int(r.findtext("acc_type")), "acc_type_nm": r.findtext("acc_type_nm")} for r in rows]
+        result = []
+        for r in rows:
+            result.append({
+                "acc_type": r.findtext("acc_type"),
+                "acc_type_nm": r.findtext("acc_type_nm")
+            })
+        return result
     except Exception as e:
         print(f"[ERROR] AccMainCode XML 파싱 실패: {e}")
         return []
 
 # API 호출
-def fetch_regioninfo():
+def fetch_outbreak_code_info():
     url = f"http://openapi.seoul.go.kr:8088/{OUTBREAK_CODE_NAME}/xml/AccMainCode/1/500/"
     res = requests.get(url, timeout=10)
     res.raise_for_status()
-    return parse_regioninfo(res.text)
+    return parse_outbreak_code_info(res.text)
 
 # MySQL 저장
-def save_regioninfo_to_mysql(region_list):
+def save_outbreak_code_info_to_mysql(outbreak_code_list):
     conn = mysql.connector.connect(**MYSQL_CONFIG)
     cursor = conn.cursor()
-    for region in region_list:
+    for outbreak_code in outbreak_code_list:
         cursor.execute("""
-            INSERT INTO OUTBREAK_DETAIL_CODE_NAME (ACC_TYPE, ACC_TYPE_NM)
+            INSERT INTO OUTBREAK_NAME (ACC_TYPE, ACC_TYPE_NM)
             VALUES (%(acc_type)s, %(acc_type_nm)s)
             ON DUPLICATE KEY UPDATE
                 ACC_TYPE_NM = VALUES(ACC_TYPE_NM)
-        """, region)
+        """, outbreak_code)
     conn.commit()
     cursor.close()
     conn.close()
-    print(f"[INFO] {len(region_list)}개의 돌발유형 코드 저장 완료")
+    print(f"[INFO] {len(outbreak_code_list)}개의 돌발유형 코드 저장 완료")
 
 # Airflow DAG에서 실행할 함수
 def update_outbreak_code_name():
     print("[INFO] update_outbreak_code_name 실행 시작")
     try:
-        regions = fetch_regioninfo()
-        if not regions:
+        outbreak_code = fetch_outbreak_code_info()
+        if not outbreak_code:
             print("[WARN] 가져온 데이터가 없습니다.")
             return
-        save_regioninfo_to_mysql(regions)
+        save_outbreak_code_info_to_mysql(outbreak_code)
         print("[INFO] update_outbreak_code_name 완료")
     except Exception as e:
         print(f"[ERROR] update_outbreak_code_name 실패: {e}")
