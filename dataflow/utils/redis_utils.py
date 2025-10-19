3c1e821d41dba1daecda70d4478afb68799c742a
dndjeh <dndjeh12@naver.com>
2025-10-19 20:24:01 +0900

    fix : host를 docker 기준으로 redis 컨테이너로 변경

--- dataflow/utils/redis_utils.py
+++ dataflow/utils/redis_utils.py
@@ -2,7 +2,7 @@ import redis
 import json
 
 class RedisClient:
-    def __init__(self, host="localhost", port=6379, db=0):
+    def __init__(self, host="redis", port=6379, db=0):
         """
         Redis 연결 초기화
         """
