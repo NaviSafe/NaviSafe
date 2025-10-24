18d18ff68a0ccebaf84153e68aef27e61f4d2504
dign1120 <dign1120@naver.com>
2025-10-24 10:57:54 +0900

    feat : FCM 디바이스 토큰 저장 API 기능 추가 - entity, controller, service, repository, dto

---
+++ naviSafe-backend/src/main/java/com/naviSafe/naviSafe/domain/fcmDeviceTokenManagement/dto/FcmDeviceTokenRequestDto.java
@@ -0,0 +1,9 @@
+package com.naviSafe.naviSafe.domain.fcmDeviceTokenManagement.dto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class FcmDeviceTokenRequestDto {
+    private String deviceToken;
+}
