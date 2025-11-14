4b20d61b1598b5cabd4574f5ce21412b1fa22869
dign1120 <dign1120@naver.com>
2025-11-14 23:43:30 +0900

    update : FcmDeviceToken ERD와 동일하게 칼럼명 변경

--- naviSafe-backend/src/main/java/com/naviSafe/naviSafe/domain/fcmDeviceTokenManagement/entity/FcmDeviceToken.java
+++ naviSafe-backend/src/main/java/com/naviSafe/naviSafe/domain/fcmDeviceTokenManagement/entity/FcmDeviceToken.java
@@ -18,9 +18,9 @@ public class FcmDeviceToken {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(name = "device_token", unique = true, nullable = false)
     private String deviceToken;
 
-    @Column(nullable = false, updatable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 }
