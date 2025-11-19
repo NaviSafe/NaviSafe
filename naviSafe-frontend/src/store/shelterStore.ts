a8ec9bcd4ed6a31374be4fa9b74aafe95ad65420
dign1120 <dign1120@naver.com>
2025-11-20 00:42:38 +0900

    feat : zustand 내 대피소 정보 전역상태 생성(get, set)

---
+++ naviSafe-frontend/src/store/shelterStore.ts
@@ -0,0 +1,29 @@
+import {create} from 'zustand';
+
+export type ShelterType = {
+	shelterCode: number;
+    shelterCodeName: String;
+    shelterGpsList: ShelterGps[];
+}
+
+type ShelterGps = {
+    shelterName: String;
+    shelterAddress: String;
+    lot: number;
+    lat: number;
+    shelterCode: number;
+} 
+
+interface ShelterTypeState {
+    shelterType : ShelterType;
+    setShelterType: (shelter : ShelterType) => void;
+}
+
+export const useShelterTypeState = create<ShelterTypeState>((set) => ({
+    shelterType : {
+        shelterCode: 0,
+        shelterCodeName: "",
+        shelterGpsList: []
+    },
+    setShelterType: (shelter) => set({shelterType: shelter})
+}))
