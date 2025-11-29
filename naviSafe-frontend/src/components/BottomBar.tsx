2c8ba16963dc14f37c1469900e4c35b4644f7dce
dign1120 <dign1120@naver.com>
2025-11-29 21:44:37 +0900

    feat : 여기로 이동 버튼 클릭 시 현재 내위치, 도착지를 기반으로 경로조회 및 전역상태 저장

--- naviSafe-frontend/src/components/BottomBar.tsx
+++ naviSafe-frontend/src/components/BottomBar.tsx
@@ -1,7 +1,12 @@
+import { getPedestrianRoute } from "../api/skTmapRoute";
+import { useCurrentLocation } from "../hooks/useCurrentLocation";
+import { useRouteStore } from "../store/routeStore";
 import { useSelectedShelter } from "../store/selectedShelterStore";
 
 export const BottomBar = () => {
     const { selectedShelter } = useSelectedShelter();
+    const { getLocation } = useCurrentLocation();
+    const {setRouteCoords} = useRouteStore();
 
     if (!selectedShelter) return null;
 
@@ -12,9 +17,32 @@ export const BottomBar = () => {
         ? "14px"
         : "16px";
 
-    const handleNavigate = () => {
-        console.log(selectedShelter);
-        // TODO: 나중에 경로 찾기 기능 연결
+    const handleNavigate = async () => {
+        const currentLocation = await getLocation();
+        if(!currentLocation){
+            alert("위치 정보를 불러올 수 없습니다.");
+            return;
+        }
+
+        // actual my current coords
+        // const startCoords = { lat : currentLocation.lat, lon : currentLocation.lon};
+        // for testing = 서울시청
+        const startCoords = { lat : 37.5663, lon : 126.9779};
+        
+        const destination = { lat : selectedShelter.lat, lon : selectedShelter.lot};
+        
+        try{
+            const coords = await getPedestrianRoute(startCoords, destination);
+
+            if (!coords || coords.length === 0) {
+                alert("도보 경로 좌표가 없습니다.");
+                return;
+            }
+
+            setRouteCoords(coords);
+        } catch (err){
+            alert("도보 경로를 불러올 수 없습니다.");
+        }
     };
 
     return (
