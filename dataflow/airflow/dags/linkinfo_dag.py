57bc646eac4314eab2b959ae754f283761dc7046
dndjeh <dndjeh12@naver.com>
2025-12-15 22:45:43 +0900

    feat : link 테이블 데이터 삽입 dag 생성 및 dag 트리거 사용

---
+++ dataflow/airflow/dags/linkinfo_dag.py
@@ -0,0 +1,29 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.utils.dates import days_ago
+import sys
+sys.path.append("/opt/airflow/dataflow")
+
+from preprocessing.linkinfo_worker import run_linkinfo_worker
+
+
+with DAG(
+    dag_id="linkinfo_dag",
+    # start_date=days_ago(1),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+
+    process_linkinfo = PythonOperator(
+        task_id="process_linkinfo",
+        python_callable=run_linkinfo_worker,
+    )
+
+    trigger_batch = TriggerDagRunOperator(
+        task_id="trigger_outbreak_batch_dag",
+        trigger_dag_id="outbreak_batch_dag",
+        wait_for_completion=False,
+    )
+
+    process_linkinfo >> trigger_batch
