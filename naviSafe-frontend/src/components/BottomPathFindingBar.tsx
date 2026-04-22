95f116d58efa0618c51c15ab461f5b256a64020d
dign1120 <dign1120@naver.com>
2026-04-22 12:56:11 +0900

    feat : 출발,도착지 페이지 진입 전 기존 경로 초기화 기능

--- naviSafe-frontend/src/components/BottomPathFindingBar.tsx
+++ naviSafe-frontend/src/components/BottomPathFindingBar.tsx
@@ -1,25 +1,55 @@
 import { useSelectedShelter } from "../store/selectedShelterStore";
 import { useNavigate } from "react-router-dom";
+import { useRouteStore } from "../store/routeStore";
+import { Fragment, useState } from "react";
+
+import { RouteResetModal } from "./RouteResetModal";
+
 
 export const BottomPathFindingBar = () => {
     const { selectedShelter } = useSelectedShelter();
     const navigate = useNavigate();
+    const { routeCoords, distance, resetRoute } = useRouteStore();
+    const [open, setOpen] = useState<boolean>(false);
 
 
     if (selectedShelter) return null;
 
-    const openStartDestinationAddressPage = () => {
+    const hasRoute = routeCoords.length > 0 || distance > 0;
+
+    const handleClick = () => {
+        if (hasRoute) {
+          setOpen(true); // 기존 경로 있으면 모달
+        } else {
+          navigate("/src-dest"); // 없으면 바로 이동
+        }
+    };
+
+    const handleCancel = () => {
+        setOpen(false);
+    };
+
+    const handleConfirm = () => {
+        resetRoute();
+        setOpen(false);
         navigate("/src-dest");
-    }
+    };
 
     return (
+        <Fragment>
         <div className="absolute bottom-0 left-0 w-full bg-transparent p-4 flex flex-row-reverse justify-between items-center shadow-lg z-50">
             <button
                 className="bg-blue-500 text-white px-4 py-2 rounded-lg"
-                onClick={openStartDestinationAddressPage}
+                onClick={handleClick}
             >
                 네비찾기
             </button>
         </div>
+        <RouteResetModal
+            open={open}
+            onCancel={handleCancel}
+            onConfirm={handleConfirm}
+        />
+        </Fragment>
     );
 };
