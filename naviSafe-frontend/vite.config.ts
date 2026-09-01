21ea6c4341f3b4f855fdb7d353c6dd2dfb876fe1
dign1120 <dign1120@naver.com>
2026-09-01 13:25:29 +0000

    add production settings

--- naviSafe-frontend/vite.config.ts
+++ naviSafe-frontend/vite.config.ts
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+   host:'0.0.0.0',
+   allowedHosts:[
+    'www.navisafe.co.kr',
+    'navisafe.co.kr',
+   ],
+  },
+
   build: {
     sourcemap: false
   }
